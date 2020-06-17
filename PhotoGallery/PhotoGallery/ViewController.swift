1e7977292d3a0de36e9a21d5af53a02d273d93b5
Paolo Esposito <paolo.esposito@kalpa.it>
2020-06-17 19:31:21 +0200

    Project Init

---
+++ PhotoGallery/PhotoGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoGallery
+//
+//  Created by Admin on 17/06/2020.
+//  Copyright © 2020 Paolo Esposito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
